147362d088b45b8921355b51a761756cb4aef628
ItsJesewe <mrgoldenbroerrorminecraft@gmail.com>
2024-12-27 16:48:32 +0500

    🛠️ Update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PyQt6.QtWidgets import QApplication
 import sys
+
+from PyQt6.QtWidgets import QApplication
+
 from classes.logger import Logger
 from classes.main_window import MainWindow
 from classes.trigger_bot import CS2TriggerBot
